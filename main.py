6a6ac1c92da1f00ac0f6c946633e8c48d3a690db
GoGoMosh <KE16012001@yandex.ru>
2024-10-10 02:49:59 +0300

    Upgrade

--- main.py
+++ main.py
@@ -31,7 +31,6 @@ def text_recognition(file_path):
 
     return result
 
-
 # Функция для захвата экрана и обработки через OpenCV
 def capture_and_process_screen():
     with mss.mss() as sct:
@@ -41,14 +40,26 @@ def capture_and_process_screen():
         # Преобразуем изображение в формат, который понимает OpenCV (numpy array)
         img = np.array(screenshot)
 
-        # Преобразуем BGR изображение в RGB (опционально)
+        # Преобразуем BGR изображение в RGB
         img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
 
         # Показываем захваченный скриншот
         cv2.imshow('Screen Capture', img)
 
-        # Сохраняем изображение (по желанию)
+        # Сохраняем изображение
         cv2.imwrite(f'screenshot_{i}.png', img)
+
+# Функция для удаления фоток
+def deleting_img():
+    j = 0
+    for j in range(3):
+        try:
+            file = pathlib.Path(f"screenshot_{j}.png")
+            file.unlink()
+
+        # Если не будет нужного изображения
+        except FileNotFoundError:
+            continue
 
 # Основной цикл программы
 try:
@@ -93,11 +104,4 @@ finally:
     cv2.destroyAllWindows()
 
     # Удаление изображения
-    for i in range(3):
-        try:
-            file = pathlib.Path(f"screenshot_{i}.png")
-            file.unlink()
-
-        # Если не будет нужного изображения
-        except FileNotFoundError:
-            continue
+    deleting_img()
