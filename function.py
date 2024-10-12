2d0501b31d1345fcd35175c3655d20cb0ffc0791
GoGoMosh <KE16012001@yandex.ru>
2024-10-12 23:42:27 +0300

    Upgrade

--- function.py
+++ function.py
@@ -8,7 +8,7 @@ import mss
 import pathlib
 import cv2
 
-# Функция для поиска текста на фото с помощью OCR (оптическое распознавание текста)
+# Функция для поиска текста на фото с помощью OCR
 def text_recognition(file_path):
     reader = easyocr.Reader(['ru'])
     result = reader.readtext(file_path, detail=0)
