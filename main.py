7abc2b642067beb03f52acedf5ea0601a6357446
GoGoMosh <KE16012001@yandex.ru>
2024-10-28 23:17:04 +0300

    Upgrade

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import logging
 
 def main():
 
-    # Чтобы не выводилась постоянно сообщение о CUDA от Easyocrк
+    # Чтобы не выводилась постоянно сообщение о CUDA от Easyocr
     logging.getLogger('easyocr').setLevel(logging.ERROR)
 
     # Определяем размеры и положение области экрана для скриншота
